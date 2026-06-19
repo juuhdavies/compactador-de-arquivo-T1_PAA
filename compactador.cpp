c17906256ec750e66b1b340f4a3a3b5807cacf6b
juuh davies <juuh.davies12@gmail.com>
2026-06-19 16:34:25 -0300

    mini correções

--- compactador.cpp
+++ compactador.cpp
@@ -24,25 +24,41 @@ int main(){
         mostrarMenu();
         cin >> opcao;
 
-        cout <<"Digite nome do arquivo de entrada: ";
-        cin >> inputFile;
-        cout << "Digite nome do arq de saida: ";
-        cin >> outputFile;
+        
 
         // Contagem de tempo de execução da compressão/descompressão
         auto start = chrono::high_resolution_clock::now();
 
         switch(opcao){
+            case 0:
+                cout << "Saindo..." << endl;
+                break;
             case 1:
+                cout <<"Digite nome do arquivo de entrada: ";
+                cin >> inputFile;
+                cout << "Digite nome do arq de saida(de preferencia .bin): ";
+                cin >> outputFile;
                 CharHuffman::compress(inputFile, outputFile);
                 break;
             case 2:
+                cout <<"Digite nome do arquivo de entrada: ";
+                cin >> inputFile;
+                cout << "Digite nome do arq de saida: ";
+                cin >> outputFile;
                 CharHuffman::decompress(inputFile, outputFile);
                 break;
             case 3:
+                cout <<"Digite nome do arquivo de entrada: ";
+                cin >> inputFile;
+                cout << "Digite nome do arq de saida (de preferencia .bin): ";
+                cin >> outputFile;
                 WordHuffman::compress(inputFile, outputFile);
                 break;
             case 4:
+                cout <<"Digite nome do arquivo de entrada: ";
+                cin >> inputFile;
+                cout << "Digite nome do arq de saida: ";
+                cin >> outputFile;
                 WordHuffman::decompress(inputFile, outputFile);
                 break;
             default:
